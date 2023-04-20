990714532be74764114f196498df6748761c1101
vineethvijaych <131109799+vineethvijaych@users.noreply.github.com>
2023-04-20 09:29:48 +0530

    adding files

---
+++ while loop.py
@@ -0,0 +1,14 @@
+print("hello\n""hello\n""hello\n""hello") 
+print("hello\n"*5)
+#initialization
+a=1
+while a<11:
+    print("hello")
+    a+=1
+b=1
+while b<11:
+    if b%2==0:
+        print(b)
+    b+=1
+    
+   
