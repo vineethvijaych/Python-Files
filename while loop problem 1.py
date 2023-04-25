6323aaada955cadf814a34977b4f35e32e3a0596
vineeth <vineeth@gmail.com>
2023-04-25 09:30:03 +0530

    new while loop program files adding

---
+++ while loop problem 1.py
@@ -0,0 +1,15 @@
+a=int(input("enter number 1:"))
+b=int(input("enter number 2:"))
+if a<b:
+    e=a
+    while e<b+1:
+        print(e)
+        e+=1
+else:
+    e=b
+    while e<a+1:
+        print(e)
+        e+=1
+   
+                        
+    
