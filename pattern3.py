cf85a63fcfc80ae62e39a6d18a7ece738c195b8c
vineeth <vineethvijaysynnefo@gmail.com>
2023-05-19 09:06:13 +0530

    patterns qstns

---
+++ pattern3.py
@@ -0,0 +1,11 @@
+a=5
+for i in range(a):
+    print(" "*i+"*"*(a-i))
+    
+for i in range(a):
+    print(" "*i+" *"*(a-i))
+    
+b=6
+for i in range(b):
+    print(" "*(b-i)+" *"*i)
+   
---
+++ pattern7.py
@@ -0,0 +1,7 @@
+b=5
+for i in range(b):
+    print("  "*(b-i)+"  * "*i)
+a=5
+for i in range(a):
+    print("  "*i+"  * "*(a-i))
+    
