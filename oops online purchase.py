702fbfcf520a27499309f828b7ba69c5fc1e557c
vineeth <vineethvijaysynnefo@gmail.com>
2023-06-05 09:03:54 +0530

    oops programs

--- oops online purchase.py
+++ oops online purchase.py
@@ -1,1 +1,23 @@
-#
+#online purchasing
+print("available products")
+a={'tshirt':500,'jeans':800,'watch':2000,'sunglass':300}
+k=a.keys()
+v=a.values()
+for i in a:
+    print(i,a[i])
+l=[]
+for i in range(1,11):   
+    e=input("enter product:")
+    l.append(e)      
+    if e=="stop":
+        break
+    print(l)
+
+
+    
+        
+
+        
+
+
+    
